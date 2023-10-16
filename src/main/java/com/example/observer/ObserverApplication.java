8e09dc5bbf63f486bfae130bf71dde262911ac12
Jan Groot <j.j.groot@gmail.com>
2023-10-16 16:06:38 +0200

    ☠️ adding Tracer bean to context. (and version 1.0.2-SNAPSHOT)
    Run still fails. NOw with the original error.

--- src/main/java/com/example/observer/ObserverApplication.java
+++ src/main/java/com/example/observer/ObserverApplication.java
@@ -1,13 +1,20 @@
 package com.example.observer;
 
+import io.micrometer.tracing.Tracer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ObserverApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ObserverApplication.class, args);
+    }
+
+    @Bean
+    public Tracer defaultTracer() {
+        return Tracer.NOOP;
     }
 
 }
